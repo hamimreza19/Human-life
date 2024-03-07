a7a4955fd931dc325bca1fac9ff282630f8dbf35
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-03-07 10:48:25 +0600

    Add files via upload

---
+++ coding.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+ float width,length,area;
+ printf("Enter the width and length : ");
+ scanf("%d",&width,&length);
+
+area= width*length;
+printf("Area of rectangle : %d\n",area);
+
+return 0;
+
+}
