194e3c87b6d888c4fa1200bec1b2b6c07890a142
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-03-11 10:45:20 +0600

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch[30] = "The tribal cheif Hamim Reza";
+    double a =20.345;
+    int b = 5;
+    float p=8.5743,q=6.78,sum,sub,mul,div,mod;
+    sum = p+q;
+    sub=p-q;
+    mul=p*q;
+    div=p/q;
+
+
+    cout<<"Your name is : "<<ch;
+    cout<<"\nDouble number is : "<<a;
+    cout<<"\nInteger number is : "<<b;
+    cout<<"\nThe sum is : "<<sum;
+    cout<<"\nThe subtraction is : "<<sub;
+    cout<<"\nThe Multiplication is : "<<mul;
+    cout<<"\nThe Division is : "<<div;
+
+    return 0;
+}
