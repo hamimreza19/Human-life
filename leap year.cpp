9ca955cf8b5848f97950a7ba5d65d33dcc4fc19a
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-03-12 23:30:18 +0600

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int year;
+  cout<<"Enter a year : ";
+  cin>>year;
+  if((year%4==0 && year%100!=0) ||(year%400==0) )
+        cout<<"Leap year";
+
+  else
+    cout<<"Not Leap year";
+
+
+
+
+}
