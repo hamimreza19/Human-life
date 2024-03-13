2e1543dbaef8dc6ed79a5ef0339a67ca58c438c4
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-03-13 11:24:38 +0600

    Add files via upload

---
+++ Conditional operator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1, num2, num3;
+
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+    int max = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
+
+    cout<<max;
+
+    return 0;
+}
+
+
+
+
+
