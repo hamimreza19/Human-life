40f2d7ee5f8548683cf87d618c6f45df9884b799
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-05-11 11:09:05 +0600

    Add files via upload

---
+++ Factorial recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    //base case
+    if(n==1)
+        return 1;
+    else
+        return n * fact(n-1);
+}
+int main()
+{
+    int factorial = fact(4);
+
+    cout<<factorial;
+}
