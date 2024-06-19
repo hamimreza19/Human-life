684d2c6e2565e71ffe3142c3b347f66ec66f6ae9
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-06-19 19:55:39 +0600

    Add files via upload

---
+++ myfirstclass.cpp
@@ -0,0 +1,13 @@
+#include "myfirstclass.h"
+#include<iostream>
+using namespace std;
+
+MyFirstClass :: MyFirstClass()
+{
+    cout<<"Inside the consturctor"<<endl;
+}
+
+void MyFirstClass :: display()
+{
+    cout<<"Inside the display function"<<endl;
+}
