89e79c947bc80de0b97c519c5e51c97b23364dad
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-06-20 11:53:28 +0600

    Add files via upload

---
+++ myfirstclass.h
@@ -0,0 +1,14 @@
+#ifndef MYFIRSTCLASS_H
+#define MYFIRSTCLASS_H
+#include "myfirstclass.h"
+
+class MyFirstClass
+{
+    public:
+        MyFirstClass();
+        void display();
+
+
+};
+
+#endif // MYFIRSTCLASS_H
