a86144b0cf34a2e78c97540f4f23467b1376a59c
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-07-27 22:15:49 +0600

    Add files via upload

---
+++ Selection operator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+class Addition
+{
+public :
+    int c;
+
+    void learn()
+    {
+        cout<<"We are going to learn selection operator "<<endl;
+    }
+};
+int main()
+{
+ Addition ob;
+ Addition *p;
+
+ p->learn();
+
+}
