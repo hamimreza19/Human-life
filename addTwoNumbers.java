30713168aa0094897be75402453dbd0cae0a257a
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-11-06 13:12:46 +0600

    Add files via upload

---
+++ addTwoNumbers.java
@@ -0,0 +1,7 @@
+public class addTwoNumbers {
+  public static void main(String[] args) {
+    int a = 5, b = 15, sum;
+    sum = a + b;
+    System.out.println("Sum of these numbers: " + sum);
+  }
+}
