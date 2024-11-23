bf1503e11ddcd2d09b15fd8d77021e9b7cce62da
Hamim Reza <162487048+hamimreza19@users.noreply.github.com>
2024-11-23 23:24:16 +0600

    Add files via upload

---
+++ ending insertion linked list.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+     node* next;
+};
+
+int main()
+{
+    node *a = NULL, *b = NULL, *c = NULL;
+
+    a = new node();
+    b = new node();
+    c = new node();
+
+    a->data = 10;
+    b->data = 20;
+    c->data = 30;
+
+    a->next = b;
+    b->next = c;
+    c->next = NULL;
+
+    node *head = a;
+    node *hamim=head;
+
+ cout<<"Before inserting data : ";
+    while (hamim!= NULL)
+    {
+        cout << hamim->data << endl;
+       hamim = hamim->next;
+
+    }
+
+    node *temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    node *p=new node();
+    p->data=60;
+
+    temp->next = p;
+    p->next = NULL;
+cout<<"After inserting data : ";
+
+while (head!= NULL)
+    {
+        cout << head->data << endl;
+       head = head->next;
+
+    }
+    return 0;
+}
+
+
